e1b30a79ddbaed9a90693bab8c48ff490ab5e8fe
Krunoslav Bilicic <krunoslav.bilicic.ext@deliveryhero.com>
2019-02-14 21:16:06 +0100

    Removing VTX support

--- TELEMETRY/KB.lua
+++ TELEMETRY/KB.lua
@@ -1,5 +1,3 @@
-
-  
 --###############################################################
 -- TelemtryValue class
 --###############################################################
@@ -77,8 +75,6 @@ local settings = {
 --end 
 
 
-
-
 local telemetry = {}
 telemetry.battsum = TelemetryValue:new('VFAS', 'Battery')
 telemetry.a4      = TelemetryValue:new('A4',   'Cell voltage')
@@ -101,33 +97,6 @@ menu.currentItem = 1 -- main menu last selected item
 menu.previousItem = 1 -- main menu previous selected item
 
 local data = {}
---data.vfasid =       TelemetryValue.getTelemetryId("VFAS")
---data.A4id =         TelemetryValue.getTelemetryId("A4")
---data.celsid =       TelemetryValue.getTelemetryId("Cels")
---data.gpsaltid =     TelemetryValue.getTelemetryId("GAlt") 
---data.altid =        TelemetryValue.getTelemetryId("Alt") 
---data.spdid =        TelemetryValue.getTelemetryId("GSpd")
---data.gpsid =        TelemetryValue.getTelemetryId("GPS")
---data.currentid =    TelemetryValue.getTelemetryId("Curr")
---data.rssiId =       TelemetryValue.getTelemetryId("RSSI")
---data.headingid =    TelemetryValue.getTelemetryId("Hdg")
---data.fuelid =       TelemetryValue.getTelemetryId("Fuel")
---data.tmp2id =       TelemetryValue.getTelemetryId("Tmp2")
---data.tmp1id =       TelemetryValue.getTelemetryId("Tmp1")
-
-
-  
-
-
-  --init Telemetry Variables 
-  --data.battsum =    0
-  --data.cellnum =    0
-  --data.alt =        0
-  --data.galt =       0
-  --data.spd =        0
-  --data.current =    0
-  --data.rssi =       0
-  --data.heading =    0
   data.cellCount =  0
   data.maxVoltage = 0
   data.cellVoltage =0
@@ -172,20 +141,6 @@ local DisplayTimer=0
 -- function Reset Variables
 -------------------------------------------------------------------------------
 local function ResetVar() 
-  --data.battsumid =    getTelemetryId("VFAS")
-  --data.vfasid =       TelemetryValue.getTelemetryId("VFAS")
-  --data.A4id =         TelemetryValue.getTelemetryId("A4")
-  --data.celsid =       TelemetryValue.getTelemetryId("Cels")
-  --data.gpsaltid =     TelemetryValue.getTelemetryId("GAlt") 
-  --data.altid =        TelemetryValue.getTelemetryId("Alt") 
-  --data.spdid =        TelemetryValue.getTelemetryId("GSpd")
-  --data.gpsid =        TelemetryValue.getTelemetryId("GPS")
-  --data.currentid =    TelemetryValue.getTelemetryId("Curr")
-  --data.rssiId =       TelemetryValue.getTelemetryId("RSSI")
-  --data.headingid =    TelemetryValue.getTelemetryId("Hdg")
-  --data.fuelid =       TelemetryValue.getTelemetryId("Fuel")
-  --data.tmp2id =       TelemetryValue.getTelemetryId("Tmp2")
-  --data.tmp1id =       TelemetryValue.getTelemetryId("Tmp1")
   data.cellCount = 0
   data.cellVoltage = 0
   data.maxVoltage = 0
@@ -319,28 +274,6 @@ local function drawShape2Scrolled(x, y, shape, rotation, scale)
       SOLID, FORCE
     )
   end
-end
-
-
-
---###############################################################
--- function Get new Telemetry Value
---###############################################################
-local function GetnewTelemetryValue()
-  local getValue = getValue --faster
-  --local cellResult = getValue(data.celsid)
-  --data.battsum =    getValue(data.vfasid)
-  --data.A4 =         getValue(data.A4id)
-  --data.galt =       getValue(data.gpsaltid)
-  --data.alt =        getValue(data.altid)
-  --data.spd =        getValue(data.spdid)
-  --data.current =    getValue(data.currentid)
-  --data.rssi =       getValue(data.rssiId)
-  --data.gps =        getValue(data.gpsid)
-  --data.heading =    getValue(data.headingid)
-  --data.fuel =       getValue(data.fuelid)
-  --data.Tmp2 =       getValue(data.tmp2id)
-  --data.Tmp1 =       getValue(data.tmp1id)
 end
 
 
@@ -804,99 +737,6 @@ end
 
 
 
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
---- VTX
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
--- Home path
-SCRIPT_HOME = "/SCRIPTS/TELEMETRY/VTx"
-
-LOCAL_SENSOR_ID  = 0x0D
-SMARTPORT_REMOTE_SENSOR_ID = 0x1B
-FPORT_REMOTE_SENSOR_ID = 0x00
-REQUEST_FRAME_ID = 0x30
-REPLY_FRAME_ID   = 0x32
-HORUS = LCD_W >= 480
-local fa = HORUS and 2 or 1
-
-MenuBox = { x=15 * fa, y=12 * fa, w=100 * fa, x_offset=36 * fa, h_line=8 * (fa * 1.5), h_offset=3 * fa }
-SaveBox = { x=15 * fa, y=12 * fa, w=100 * fa, x_offset=4 * fa,  h=30 * fa, h_offset=5 * fa }
-NoTelem = { 30 * fa, 55 * fa, "No Telemetry", BLINK }
-
-protocol = {
-	rssi            = function() return getValue("RSSI") end,
-	exitFunc        = function() return 0 end,
-	stateSensor     = "Tmp1",
-	push            = sportTelemetryPush,
-	maxTxBufferSize = 6,
-	maxRxBufferSize = 6,
-	saveMaxRetries  = 2,
-	saveTimeout     = 300
-}
-
-protocol.mspSend = function(payload)
-	local dataId = 0
-	dataId = payload[1] + bit32.lshift(payload[2],8)
-	local value = 0
-	value = payload[3] + bit32.lshift(payload[4],8)
-		+ bit32.lshift(payload[5],16) + bit32.lshift(payload[6],24)
-	return sportTelemetryPush(LOCAL_SENSOR_ID, REQUEST_FRAME_ID, dataId, value)
-end
-
-protocol.mspRead = function(cmd)
-	return mspSendRequest(cmd, {})
-end
-
-protocol.mspWrite = function(cmd, payload)
-	return mspSendRequest(cmd, payload)
-end
-
-protocol.mspPoll = function()
-	local sensorId, frameId, dataId, value = sportTelemetryPop()
-	if (sensorId == SMARTPORT_REMOTE_SENSOR_ID or sensorId == FPORT_REMOTE_SENSOR_ID) and frameId == REPLY_FRAME_ID then
-		local payload = {}
-		payload[1] = bit32.band(dataId,0xFF)
-		dataId = bit32.rshift(dataId,8)
-		payload[2] = bit32.band(dataId,0xFF)
-		payload[3] = bit32.band(value,0xFF)
-		value = bit32.rshift(value,8)
-		payload[4] = bit32.band(value,0xFF)
-		value = bit32.rshift(value,8)
-		payload[5] = bit32.band(value,0xFF)
-		value = bit32.rshift(value,8)
-		payload[6] = bit32.band(value,0xFF)
-		return mspReceivedReply(payload)
-	end
-	return nil
-end
-
-assert(loadScript(SCRIPT_HOME.."/common.luac", "T"))()
-collectgarbage()
-
-local run_ui = assert(loadScript(SCRIPT_HOME.."/ui.luac", "T"))()
-collectgarbage()
-
-local background = assert(loadScript(SCRIPT_HOME.."/background.luac", "T"))()
-collectgarbage()
-
-local MENU_TIMESLICE = 100
-local lastMenuEvent = 0
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-
-
-
 
 
 -- ###############################################################
@@ -932,9 +772,9 @@ end
 -- Draw screen 2
 -- ###############################################################  
 function screen2(event)
-  lastMenuEvent = getTime()
-	collectgarbage()
-  run_ui(event)
+  --lastMenuEvent = getTime()
+	--collectgarbage()
+  --run_ui(event)
 
   DrawTitleBar()
 end
@@ -1095,10 +935,10 @@ end
 --------------------------------------------------------------------------------
 local function backgroundwork()
   -- background work for betaflight VTX
-  if menu.currentItem == 2 and lastMenuEvent + MENU_TIMESLICE < getTime() then
-    background.run_bg()
-    collectgarbage()
-  end
+  --if menu.currentItem == 2 and menu.currentMenu == 0 and lastMenuEvent + MENU_TIMESLICE < getTime() then
+    --background.run_bg()
+    --collectgarbage()
+  --end
 
   -- get new values
   RefreshTelemetryValues()
